f4724ab366c8c5d5794b9683c1d86d3fc6a2bbd4
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-04-29 16:36:14 -0700

    save

--- api/auth/authRouter.spec.js
+++ api/auth/authRouter.spec.js
@@ -1,27 +1,33 @@
+process.env.DB_ENV = 'testing';
 const server = require('../server');
 const request = require('supertest');
-const db = require('../../database/dbConfig');
+const db = require('../../database/dbConfig')
+const knex = require('knex');
 
 describe('/auth', () => {
 	describe('/register', () => {
-    // beforeEach(async () => {
-    //   await db('Users').truncate(); // empty table and reset ids
-    // });
+		beforeEach(() => {
+			knex(db['testing']).migrate.rollback()
+      .then(() => knex.migrate.latest())
+      .then(() => knex.seed.run())
+		});
 
 		test('should return a invalid register attempt', () => {
 			return request(server)
-        .post('/api/auth/register')
-        .send({ username: 'a', password: '123' })
+				.post('/api/auth/register')
+				.send({ username: 'a', password: '123' })
 				.then(res => {
 					expect(res.status).toBe(401);
-					expect(res.body.errorMessage).toBe(`Invalid attempt. username must contain atleast 3 characters & password must be more then 5`);
+					expect(res.body.errorMessage).toBe(
+						`Invalid attempt. username must contain atleast 3 characters & password must be more then 5`
+					);
 				});
-    });
-    
-    test('should return a valid register attempt', () => {
+		});
+
+		test('should return a valid register attempt', () => {
 			return request(server)
-        .post('/api/auth/register')
-        .send({ username: 'Billy', password: 'password' })
+				.post('/api/auth/register')
+				.send({ username: 'Billy', password: 'password' })
 				.then(res => {
 					expect(res.status).toBe(201);
 				});
