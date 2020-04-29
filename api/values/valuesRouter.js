fa2902d0995b8ff9c3a5342c3ca91d09aa5b2501
david <davidealvarez1@gmail.com>
2020-04-29 10:57:30 -0700

    fixed post to value endpoint

--- api/values/valuesRouter.js
+++ api/values/valuesRouter.js
@@ -14,24 +14,42 @@ router.get('/', (req, res) => {
 router.post('/', (req,res) =>{
 	console.log("req.body: ",req.body)
 	Values.add(req.body)
-		.then(res => {
-			res.status(201).json(res)
-		})
-		.catch(err => {
-			console.error(err)
-			res.status(500).json(err)
-		})
+	.then(value => {
+		console.log("value: ", value)
+        res.status(200).json(value)
+    })
+    .catch(error => {
+    	console.log(error);
+    	res.status(500).json({error: error.message});
+    })
+})
+
+//post to store what users' 3 values are
+router.post('/:id', (req,res) =>{
+
 })
 
 router.put('/:id', (req,res) => {
 	const id = req.params.id;
 	Values.update(id, req.body)
 		.then(res => {
+			console.log("res: ",res)
 			res.status(200).json(res)
 		})
 		.catch(err => {
-			res.status(500).json(err)
+			res.status(500).json(res.body.food)
 		})
 })
-// router.delete('/')
+
+router.delete('/:id', (req, res) => {
+	const id = req.params.id;
+	Values.remove(id)
+		.then(res => {
+			res.status(200).json(res)
+		})
+		.catch(err => {
+			console.error(err)
+			res.status(500).json({message: 'could not delete specified value'})
+		})
+})
 module.exports = router;
